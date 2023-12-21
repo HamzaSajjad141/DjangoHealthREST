12742e00e1daab488c49fc091f61ea9a76cfd7d9
HamzaSajjad141 <hamzasajjad1277@gmail.com>
2023-12-22 04:52:08 +0500

    add-endpoints

--- djangoproject/api/urls.py
+++ djangoproject/api/urls.py
@@ -1,4 +1,3 @@
-# api/urls.py
 from django.urls import path
 from . import views
 
@@ -6,4 +5,9 @@ urlpatterns = [
     path('', views.GetAllData.as_view()),
     path('smokers/<region>/', views.GetSmokersByRegion.as_view()),
     path('getbyid/<int:id>/', views.GetDataById.as_view()),
+    path('create/', views.CreateData.as_view()),
+    path('updatebyid/<int:id>/', views.UpdateDataById.as_view()),
+    path('deletebyid/<int:id>/', views.DeleteDataById.as_view()),
+    path('getbyagerange/<int:min_age>/<int:max_age>/', views.GetDataByAgeRange.as_view(),),
+    path('getbygender/<str:gender>/', views.GetDataByGender.as_view()),
 ]
--- djangoproject/api/views.py
+++ djangoproject/api/views.py
@@ -30,3 +30,76 @@ class GetSmokersByRegion(APIView):
         return Response(serializer.data)
 
 
+
+class CreateData(APIView):
+    def post(self, request):
+        serializer = HealthInsaurenceDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateDataById(APIView):
+    def put(self, request, id):
+        try:
+            data = HealthInsaurenceData.objects.get(id=id)
+        except HealthInsaurenceData.DoesNotExist:
+            return Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = HealthInsaurenceDataSerializer(data, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class DeleteDataById(APIView):
+    def delete(self, request, id):
+        try:
+            data = HealthInsaurenceData.objects.get(id=id)
+        except HealthInsaurenceData.DoesNotExist:
+            return Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+class GetDataByAgeRange(APIView):
+    def get(self, request, min_age, max_age):
+        try:
+            min_age = int(min_age)
+            max_age = int(max_age)
+        except ValueError:
+            return Response({"error": "Invalid age range. Please provide valid integers for min_age and max_age."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if min_age < 0 or max_age < 0:
+            return Response({"error": "Age values cannot be negative."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if max_age < min_age:
+            return Response({"error": "max_age must be greater than or equal to min_age."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        data = HealthInsaurenceData.objects.filter(age__gte=min_age, age__lte=max_age)
+        serializer = HealthInsaurenceDataSerializer(data, many=True)
+        return Response(serializer.data)
+
+
+class GetDataByGender(APIView):
+    def get(self, request, gender):
+        if gender.lower() not in ['male', 'female']:
+            return Response({"error": "Invalid gender. Please provide 'male' or 'female'."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        data = HealthInsaurenceData.objects.filter(sex=gender.lower())
+        serializer = HealthInsaurenceDataSerializer(data, many=True)
+        return Response(serializer.data)
+
+
+
+
